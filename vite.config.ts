0a6d1650728ab6e3ea907babe39a35d5656ac810
karthikarun172 <57855758+karthikarun172@users.noreply.github.com>
2025-07-20 10:08:18 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "karthikarun172", // Replace with your actual repo name
+  base: "/FrontEndTask/", // Replace with your actual repo name
 });
